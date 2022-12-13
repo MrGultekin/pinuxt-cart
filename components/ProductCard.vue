71203ac7736ee13d5758e66bf148605de4478def
MrGultekin <gultekin.de@gmail.com>
2022-12-13 12:41:53 +0100

    ProductCard component added.

---
+++ components/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card flex items-center gap-8">
+    <img :src="product.img" :alt="product.title">
+    <div>
+      <p class="text-2xl text-secondary">{{ product.title }}</p>
+      <p class="text-xl text-gray-50">{{ product.description }}</p>
+      <p class="text-lg text-secondary my-3">{{ product.price }} Silver coins</p>
+      <button class="btn">
+        <span>Add to basket</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// export default {
+//   name: "ProductCard" asagidaki gibi tercih edilir yeni yöntem
+// } Bu bir prop alacak componets da defineProps ile tanımlayip define ederiz nuxtda,extract ediyoruz
+const {product} = defineProps(['product'])
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,16 @@
     <h2 class="text-white text-2xl text-center my-8">All Products</h2>
     <div class="grid grid-cols-2 gap-7">
       <div v-for="product in products" :key="product.id">
-{{product.title}}
+        <!--        {{ product.title }}-->
+        <ProductCard :product="product"/>
+
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const { data: products } = await useFetch('http://localhost:4000/products')
-
-
+const {data: products} = await useFetch('http://localhost:4000/products')
 </script>
 
 <style scoped>
