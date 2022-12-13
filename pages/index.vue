7de62c8f05258e7ee1645a4838b65874c8f15c3a
MrGultekin <gultekin.de@gmail.com>
2022-12-13 11:30:15 +0100

    json-server added and works now

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">All Products</h2>
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products" :key="product.id">
+{{product.title}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+const { data: products } = await useFetch('http://localhost:4000/products')
+
 
 </script>
 
