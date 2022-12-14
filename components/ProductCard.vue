108c9c89d90161d394e815875a778368e1a8a173
MrGultekin <gultekin.de@gmail.com>
2022-12-14 10:26:24 +0100

    adding Loading State works and wait a sec then adds the product to the basket

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -7,8 +7,9 @@
       <p class="text-xl text-gray-50">{{ product.description }}</p>
       <p class="text-lg text-secondary my-3">{{ product.price }} Silver coins</p>
      <!-- <button class="btn" @click="cartStore.addToCart(product)" >  Way 2 call it indirectly in order to use LoadState -->
-      <button class="btn" @click="cartStore.addToCart(product)" >
-        <span >Add to basket</span>
+      <button class="btn" :disabled="isPending" @click="addToBasket()" >
+        <span v-show="!isPending">Add to basket</span>
+        <span v-show="isPending">Adding....</span>
       </button>
     </div>
 
@@ -20,10 +21,15 @@
 //   name: "ProductCard" asagidaki gibi tercih edilir yeni yöntem
 // } Bu bir prop alacak componets da defineProps ile tanımlayip define ederiz nuxtda,extract ediyoruz
   import {useCartStore} from "~/stores/cartStore";
-
 const cartStore = useCartStore()
-const addToBasket = () => {
-  cartStore.addToCart(product)
+
+const isPending = ref(false);
+const addToBasket = async () => {
+  isPending.value = true;
+  await cartStore.addToCart(product)
+  setTimeout(() => {
+    isPending.value = false;
+  }, 1000)
 }
 const {product} = defineProps(['product'])
 
